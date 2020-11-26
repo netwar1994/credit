9b236a9aa6135a4d4612b7937643de474c8a2230
Tilek Asankulov <tilekasankulov04@gmail.com>
2020-11-26 13:49:04 +0600

    fixed return value

--- cmd/credit/main.go
+++ cmd/credit/main.go
@@ -5,8 +5,9 @@ import (
 	"github.com/netwar1994/credit/pkg/credit"
 )
 
-
-
 func main() {
-	fmt.Println(credit.Calculate(1_000_000_00, 36, 20))
+	monthlyPayment, creditOverpayment, totalPayment := credit.Calculate(1_000_000_00, 36, 20)
+	fmt.Println("Ежемесячный платеж:", monthlyPayment)
+	fmt.Println("Переплата по кредиту:", creditOverpayment)
+	fmt.Println("Общая выплата:", totalPayment)
 }
--- pkg/credit/credit.go
+++ pkg/credit/credit.go
@@ -1,20 +1,15 @@
 package credit
 
 import (
-	"fmt"
 	"math"
 )
 
 func Calculate(amount, period, percent int64) (monthly int64, overpayment int64, total int64) {
 	monthlyCreditRate := float64(percent) / 12 / 100
-	fmt.Println(monthlyCreditRate)
 	allPeriodCreditRate := math.Pow(1 + monthlyCreditRate, 36)
-	fmt.Println(allPeriodCreditRate)
 	annuityRatio := monthlyCreditRate * allPeriodCreditRate / (allPeriodCreditRate - 1)
-	fmt.Println(annuityRatio)
 	monthly = int64(annuityRatio * float64(amount))
-	overpayment = monthly * period - amount
-	total = monthly * period
-	fmt.Println(amount * 100)
+	overpayment = int64(annuityRatio * float64(amount) * float64(period)) - amount
+	total = int64(annuityRatio * float64(amount) * float64(period))
 	return
 }
